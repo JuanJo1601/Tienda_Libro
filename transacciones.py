0e8a7ccc72768224603be50b1b9758c7195450d6
JuanJo1601 <161753224+JuanJo1601@users.noreply.github.com>
2024-05-09 09:48:00 -0500

    Update transacciones.py

--- transacciones.py
+++ transacciones.py
@@ -1,31 +1,31 @@
-
 class Transaccion:
 
-    '''-------------------
-    Atributos
-    -------------------'''
-
-    fecha = ''
     TIPO = {
         'abast' : 'Abastecimiento',
         'venta' : 'Venta'
     }
 
-    '''-------------------
-    Metodos
-    -------------------'''
-
-    def __init__(self, pFecha, pTipo, pCantidad):
-        self.__fecha = pFecha
-        self.__tipo = (self.TIPO.abast, self.TIPO.venta)[pTipo]
-        self.__cantidad = pCantidad
+    def __init__(self, fecha, tipo, cantidad):
+        
+        self.__fecha = fecha
+        self.__tipo = self.TIPO['1'] if tipo == 1 else self.TIPO['2']
+        self.__cantidad = cantidad
 
     def getTipo(self):
         return self.__tipo
 
+    def setTipo(self, tipo):
+        self.__tipo = self.TIPO['1'] if tipo == 1 else self.TIPO['2']
+
     def getFecha(self):
         return self.__fecha
     
+    def setFecha(self, fecha):
+        self.__fecha = fecha
+    
     def getCantidad(self):
         return self.__cantidad
+    
+    def setCantidad(self, cantidad):
+        self.__cantidad = cantidad
     
